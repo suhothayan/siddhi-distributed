93c1174949839715c9dbad6d0cea3b76040c0bb1
suho <suhothayan@gmail.com>
2017-09-22 18:24:16 +0530

    add test

--- src/test/java/org/suho/siddhi/distributed/TestCase.java
+++ src/test/java/org/suho/siddhi/distributed/TestCase.java
@@ -13,6 +13,8 @@ public class TestCase {
         System.out.println("Hi 123");
         System.out.println("Java home : " + System.getenv("JAVA_HOME"));
         System.out.println("foo : " + System.getenv("foo").equals("bar"));
+        System.out.println("foo User : " + System.getenv("fooUser").equals("foo1"));
+        System.out.println("foo password : " + System.getenv("fooPassword").equals("bar1"));
 
     }
 }
