5b8e5ef31a0021b73658f4fbbae2c1f479a10d9f
suho <suhothayan@gmail.com>
2017-09-22 17:54:52 +0530

    add test

---
+++ src/test/java/org/suho/siddhi/distributed/TestCase.java
@@ -0,0 +1,17 @@
+package org.suho.siddhi.distributed;
+
+import org.junit.Test;
+
+/**
+ * Created by suho on 9/22/17.
+ */
+public class TestCase {
+
+    @Test
+    public void testFoo() {
+
+        System.out.println("Hi 123");
+        System.out.println("Java home : " + System.getProperty("JAVA_HOME"));
+
+    }
+}
